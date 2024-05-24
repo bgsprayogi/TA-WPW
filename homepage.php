1bd52755fcc9087a760c7b1afc42fe864bf133b2
bgsprayogi <prayogigres@gmail.com>
2024-05-24 19:29:32 +0700

    nambah login page

---
+++ homepage.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="homepage.css" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div class="header">
+      <ul class="ul-header">
+        <li class="li-header1">
+          <img id="logo" src="logo new.png" alt="" />
+        </li>
+        <li class="li-header2">Home</li>
+        <li class="li-header3">Product</li>
+        <li class="li-header4">Login</li>
+      </ul>
+    </div>
+
+    <div class="navbar p-0">
+      <img src="background.png" id="bgnavbar" />
+      <div id="navbar_box">
+        <p class="text_home1">WELCOME to TECHive</p>
+      </div>
+      <div id="navbar_box">
+        <p class="text_home2">
+          At TECHive,<br />
+          <br />
+          We believe shopping should be a delightful experience, not a chore.
+          <br />
+          That's why we're committed to offering you the best products, prices,
+          and service.<br />
+          Whether you're looking for the latest fashion, cutting-edge
+          electronics, or something unique to brighten your day, we've got you
+          covered.
+        </p>
+      </div>
+    </div>
+
+    <div id="middle_box">
+      <div>
+        <p class="middle_text">we are Techive,The always there for you</p>
+      </div>
+    </div>
+
+    <div class="benefit_box">
+      <div class="row row-cols-2 row-cols-4 g-2 g-lg-3">
+
+        <div class="col">
+          <div class="card shadow mb-3" style="width: 18rem">
+            <div class="card-body">
+              <h5 class="card-title text-center">Guaranteed Product Quality</h5>
+              <p class="card-text">
+                Techive  menyediakan produk-produk berkualitas tinggi dari merek-merek terkenal dan terpercaya. Dengan demikian, pelanggan dapat memiliki keyakinan   
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow mb-3" style="width: 18rem">
+            <div class="card-body">
+              <h5 class="card-title text-center">Product Specialization</h5>
+              <p class="card-text">
+                Toko jual beli komputer fokus pada menyediakan produk dan layanan terkait komputer, 
+                sehingga mereka biasanya memiliki pengetahuan mendalam tentang produk-produk yang mereka jual.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow mb-3" style="width: 18rem">
+            <div class="card-body">
+              <h5 class="card-title text-center">After Sales Service</h5>
+              <p class="card-text">
+                Techive  ini memberi pelanggan jaminan bahwa mereka akan mendapatkan dukungan teknis dan bantuan jika mereka mengalami masalah dengan produk yang mereka beli di masa depan.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="card shadow mb-3" style="width: 18rem">
+            <div class="card-body">
+              <h5 class="card-title text-center" >Diverse Choices</h5>
+              <p class="card-text">
+                Techive menawarkan berbagai macam produk komputer dari berbagai merek dan model. 
+                Hal ini memberi pelanggan pilihan  luas.memungkinkan mereka untuk membandingkan berbagai fitur, 
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div  class="container1 ">
+      <div class="row gx-5">
+        <div class="col">
+         <img id="picture"   src="set_coomputer.png" alt="">
+        </div>
+        <div class="col">
+          <div id="text_middle" class="p-3  "> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</div>
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
---
+++ loginpage.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="loginpage.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="login_page">
+        <img src="set_computer.png" alt="computer" width: 100%;>
+        <div class="box">
+            <div class="anakbox">
+                <img src="logo new.png" alt="logo" style="margin-top: 30px ; margin-left: 190px">
+                <p>email</p>
+                <div class="email_box"></div>
+                <p>password</p>
+                <div class="password_box"></div>
+                <div class="signin_box">
+                    <p1>Sign in</p1>
+                </div>
+            </div>           
+        </div>
+    </div>
+</body>
+</html>
